b6ed701f08108ab01d6d15adc4f2b0971ac2da4d
Timur <fantomas2213@gmail.com>
2020-03-05 14:48:29 +0300

    checked request

--- 2/index.php
+++ 2/index.php
@@ -1,33 +1,37 @@
 <?php
 include("form.html");
-$text = (string)$_REQUEST['text'];
-function rephrase($item)
-{
-    $count = 0;
-    for($i=0;$i < strlen($item);$i++) {
-        if ($item[$i] == 'h') {
-            $item[$i] = '4';
-            $count++;
+if (isset($_REQUEST['text'])) {
+    $text = (string)$_REQUEST['text'];
+    function rephrase($item)
+    {
+        $count = 0;
+        for ($i = 0; $i < strlen($item); $i++) {
+            if ($item[$i] == 'h') {
+                $item[$i] = '4';
+                $count++;
+            }
+            if ($item[$i] == 'i') {
+                $item[$i] = '1';
+                $count++;
+            }
+            if ($item[$i] == 'e') {
+                $item[$i] = '3';
+                $count++;
+            }
+            if ($item[$i] == 'o') {
+                $item[$i] = '0';
+                $count++;
+            }
+            yield $item[$i];
         }
-        if ($item[$i] == 'i') {
-            $item[$i] = '1';
-            $count++;
-        }
-        if ($item[$i] == 'e') {
-            $item[$i] = '3';
-            $count++;
-        }
-        if ($item[$i] == 'o') {
-            $item[$i] = '0';
-            $count++;
-        }
-        yield $item[$i];
+        return $count;
     }
-    return $count;
+
+    $generator = rephrase($text);
+    foreach ($generator as $val) {
+        echo $val;
+    }
+    echo "<br>" . " return = " . $generator->getReturn();
 }
-$generator = rephrase($text);
-foreach($generator as $val)
-{
-    echo $val;
-}
-echo "<br>"." return = ".$generator->getReturn();
+else
+    echo "<p>" . "input is empty!" . "</p>";
