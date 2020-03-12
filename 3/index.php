5cb41f9d427062b376b836815a53dae181447a84
Timur <fantomas2213@gmail.com>
2020-03-12 16:59:02 +0300

    correct ex 3

--- 3/index.php
+++ 3/index.php
@@ -1,24 +1,26 @@
 <?php
 include("form.html");
 if (isset($_REQUEST['input'])) {
-    $input = explode(PHP_EOL, $_REQUEST['input']);
-    $shuffleInput = array_slice($input, 0); //clone arr
-    //shuffling cloned arr
-    foreach ($shuffleInput as &$item) {
-        $explodeItem = explode(' ',$item);
-        shuffle($explodeItem);
-        $item = implode(' ', $explodeItem);
-    }
+    $input = array_map(function ($item) {
+        return explode(' ', $item);
+    }, explode(PHP_EOL, $_REQUEST['input'])); //breaking down into [[]]
 
-    $result = array_merge($input, $shuffleInput);
-    uksort($result, function ($r1, $r2) {
+    $clone_arr = array_slice($input, 0); //clone arr
+
+    array_walk($clone_arr, function (&$item) {
+        shuffle($item); //shuffling cloned arr
+    });
+
+    $result = array_merge($input, $clone_arr);
+    uasort($result, function ($r1, $r2) {
         if (isset($r1[1]) && isset($r2[1]))
             return strcmp($r1[1], $r2[1]);// comparing by 2nd words
         else return 0;
     });
 
-    foreach ($result as $out) {
+    $result = array_map(function ($item) {
+        return implode(' ', $item); // pack into string
+    }, $result);
+    foreach ($result as $out)
         echo "<br>" . $out;
-    }
-
 }
