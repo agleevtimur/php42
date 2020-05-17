1846fc2f3050a5c5fa4746d9457a03961a935426
Timur Agleev <fantomas2213@gmail.com>
2020-05-18 02:54:18 +0300

    fix index file

--- 10/Controller.php
+++ 10/Controller.php
@@ -2,81 +2,64 @@
 use Exceptions as Ex;
 class Controller implements IController
 { // 4 метода, которые случайно выкидывают 2 исключения
+  public array $Exceptions;
   private $rnd;
   function methodOne()
   {
     $this->rnd = $this->getRandom();
-    $this->throwTwoRndExceptions();
+    $this->makeTwoRndExceptions();
   }
   function methodTwo()
   {
-    $this->rnd = $this->getRandom();
-    $this->throwTwoRndExceptions();
+    $this->methodOne();
   }
   function methodThree()
   {
-    $this->rnd = $this->getRandom();
-    $this->throwTwoRndExceptions();
+    $this->methodOne();
   }
   function methodFour()
   {
-    $this->rnd = $this->getRandom();
-    $this->throwTwoRndExceptions();
+    $this->methodOne();
   }
   private function getRandom():int
   {
     return rand(1,10);
   }
-  private function throwTwoRndExceptions()
+  private function makeTwoRndExceptions()
   { // различные попарные комбинации исключений
     switch ($this->rnd) {
       case '1':
-        $this->throwException(new Ex\ExceptionOne(), new Ex\ExceptionTwo());
+        $this->Exceptions = array(new Ex\ExceptionOne(), new Ex\ExceptionTwo());
         break;
       case '2':
-        $this->throwException(new Ex\ExceptionOne(), new Ex\ExceptionThree());
+        $this->Exceptions = array(new Ex\ExceptionOne(), new Ex\ExceptionThree());
         break;
       case '3':
-        $this->throwException(new Ex\ExceptionOne(), new Ex\ExceptionFour());
+        $this->Exceptions = array(new Ex\ExceptionOne(), new Ex\ExceptionFour());
         break;
       case '4':
-        $this->throwException(new Ex\ExceptionOne(), new Ex\ExceptionFive());
+        $this->Exceptions = array(new Ex\ExceptionOne(), new Ex\ExceptionFive());
         break;
       case '5':
-        $this->throwException(new Ex\ExceptionTwo(), new Ex\ExceptionThree());
+        $this->Exceptions = array(new Ex\ExceptionTwo(), new Ex\ExceptionThree());
         break;
       case '6':
-        $this->throwException(new Ex\ExceptionTwo(), new Ex\ExceptionFour());
+        $this->Exceptions = array(new Ex\ExceptionTwo(), new Ex\ExceptionFour());
         break;
       case '7':
-        $this->throwException(new Ex\ExceptionTwo(), new Ex\ExceptionFive());
+        $this->Exceptions = array(new Ex\ExceptionTwo(), new Ex\ExceptionFive());
         break;
       case '8':
-        $this->throwException(new Ex\ExceptionThree(), new Ex\ExceptionFour());
+        $this->Exceptions = array(new Ex\ExceptionThree(), new Ex\ExceptionFour());
       break;
       case '9':
-        $this->throwException(new Ex\ExceptionThree(), new Ex\ExceptionFive());
+        $this->Exceptions = array(new Ex\ExceptionThree(), new Ex\ExceptionFive());
         break;
       case '10':
-        $this->throwException(new Ex\ExceptionFour(), new Ex\ExceptionFive());
+        $this->Exceptions = array(new Ex\ExceptionFour(), new Ex\ExceptionFive());
       break;
       default:
         break;
-    }
-  }
-  private function throwException($ex1, $ex2)
-  {
-    try{
-      throw $ex1;
-    }
-    catch(Exception $ex1){
-      echo 'исключение: ' . $ex1->getMessage() . '<br/>';
-    }
-    try{
-      throw $ex2;
-    }
-    catch(Exception $ex2){
-      echo 'исключение: ' . $ex2->getMessage();
     }
   }
 }
--- 10/index.php
+++ 10/index.php
@@ -6,4 +6,13 @@ spl_autoload_register(function($class)
 
 $obj = new Controller();
 $obj->methodOne();
+foreach ($obj->Exceptions as $item) {
+  try{
+    throw $item;
+  }
+  catch(Exception $ex){
+    echo 'исключение: ' . $ex->getMessage() . '<br/>';
+  }
+}
+
 ?>
