49b376584957bbd1080107240d3df47f38abfdb8
agleev_timur <agleev_timur@localhost.localdomain>
2020-10-17 22:34:05 +0400

    check user-agent existence in request

--- exercise/autumn2020/curl/form/action.php
+++ exercise/autumn2020/curl/form/action.php
@@ -2,6 +2,10 @@
 
 declare(strict_types=1);
 
+if (!isset($_SERVER['HTTP_USER_AGENT'])) {
+    die("You are not browser user");
+}
+
 $name = getRequestData('name');
 $age = getRequestData('age', 0);
 $city = getRequestData('city');
@@ -10,7 +14,7 @@ $file = fopen('data.txt', 'w');
 fwrite($file, serialize(['name' => $name, 'age' => $age, 'city' => $city]));
 fclose($file);
 
-echo 'answers has been saved!';
+echo 'Answers has been saved!';
 
 function getRequestData(string $key, int|string $default = ""): int|string
 {
