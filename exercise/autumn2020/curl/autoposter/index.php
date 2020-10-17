0c5a01172effb8a5d5f7c23b1a7a0d09972a5cce
agleev_timur <agleev_timur@localhost.localdomain>
2020-10-17 18:55:00 +0400

    done autoposter

--- exercise/autumn2020/curl/autoposter/index.php
+++ exercise/autumn2020/curl/autoposter/index.php
@@ -1,2 +1,10 @@
 <?php
 
+$data = ['name' => 'autoposter', 'age' => 0, 'city' => 'kazan'];
+
+$curl = curl_init('localhost:8000/action.php');
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36');
+$res = curl_exec($curl);
+curl_close($curl);
