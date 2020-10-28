bbbc8d0fc0325198335de996317406deaa7e6548
agleevtimur <fantomas2213@gmail.com>
2020-10-28 23:35:49 +0400

    base save data done

--- exercise/autumn2020/redis/index.php
+++ exercise/autumn2020/redis/index.php
@@ -1,15 +1,40 @@
 <?php
 
-$name = getRequestData('name');
-$comment = getRequestData('comment');
+declare(strict_types=1);
+
+$redis = setRedisServer();
+
+if ($_POST !== []) {
+    $name = getRequestData('name');
+    $comment = getRequestData('comment');
+
+    if (isset($name) && isset($comment)) {
+        $redis->incr('comments_count');
+        $redis->hset('comments', $redis->get('comments_count'), serialize(['name' => $name, 'comment' => $comment]));
+    }
+}
+
+$fields = $redis->hgetall('comments');
+foreach ($fields as $field) {
+    $field = unserialize($field);
+    echo "{$field['name']} - {$field['comment']}\n";
+}
 
 
 
-
-
-
-
-function getRequestData(string $key): string
+function getRequestData(string $key): ?string
 {
-    return $_POST[$key] ?? "";
+    return $_POST[$key] ?? null;
+}
+
+function setRedisServer(): Predis\Client
+{
+    require 'vendor/autoload.php';
+    Predis\Autoloader::register();
+    try {
+        return new Predis\Client();
+    }
+    catch (Exception $e) {
+        die($e->getMessage());
+    }
 }
