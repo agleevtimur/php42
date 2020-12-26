542507dbbad06b99ef0f47241f3d4dfb81f88146
agleev_timur <agleev_timur@localhost.localdomain>
2020-12-26 19:00:41 +0400

    fix front

--- exercise/autumn2020/logger/public/script.js
+++ exercise/autumn2020/logger/public/script.js
@@ -1,13 +1,10 @@
-const redButton = document.getElementById('red');
-const greenButton = document.getElementById('green');
-const blueButton = document.getElementById('blue');
 const container = document.getElementById('container');
 
 container.addEventListener('click', async () => {
    event.preventDefault();
 
    let element = event.target;
-   let response = await fetch('http://localhost:8000/log_button', {
+   let response = await fetch('http://127.0.0.1:8000/log_button', {
        method: 'POST',
        headers: {
            'Content-Type': 'application/json',
